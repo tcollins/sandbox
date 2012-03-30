eb454c1b172d7027ffac625bebe88b71fbb9d53d
Tim Collins <tim@tcollins.net>
2012-03-29 22:17:51 -0400

    more node and socket.io stuff

--- nodejs-websocket/client/client.js
+++ nodejs-websocket/client/client.js
@@ -1,16 +1,32 @@
 var APP = {
 
-	init:		function(){					
+	init:		function(){	
+					var self =this;
+					
 					this.socket = io.connect('http://localhost:4000');
+					
+					this.msgContainer = $('#message-container');
+					
+					
+					this.socket.on('new.msg', function (data) {
+					    	console.log('new.msg', data);
+					    	self.msgContainer.prepend('<div>'+data.user + ' - ' + data.msg+'</div>');
+					});
+					
+				},
+				
+	setUsername:function(userName){
+					this.socket.emit('set.username', {      
+					      userName: userName
+    				});
 				},
 				
 	sendMsg:	function(msg){
-					this.socket.emit('private message', {      
+					this.socket.emit('send.msg', {      
 					      msg: msg
     				});
-				}
+				}			
 
 }
 
 
- 
--- nodejs-websocket/server/server.js
+++ nodejs-websocket/server/server.js
@@ -5,9 +5,22 @@ var io = require('socket.io').listen(4000);
 io.sockets.on('connection', function (socket) {
 	io.sockets.emit('this', { will: 'be received by everyone'});
 	
-	socket.on('private message', function (from, msg) {
-    	console.log('I received a private message by ', from, ' saying ', msg);
+	socket.on('set.username', function (data) {
+    	console.log('set.username', data);
+    	socket.set('username', data.userName, function () {
+			socket.emit('ready');
+    	});
   	});
+  	
+  	socket.on('send.msg', function (data) {
+  		socket.get('username', function (err, name) {
+		    console.log('Chat message by ', name);
+			console.log(data);
+	    	io.sockets.emit('new.msg', {user: name, msg: data.msg});
+   		});	    	
+  	});
+  	
+  	
 
   	socket.on('disconnect', function () {
   		io.sockets.emit('user disconnected');
